cee944dfd929c36e846cc065e72d10d400d8c202
PJ Passalacqua <pjpassa@gmail.com>
2015-07-27 14:41:25 -0400

    completed basic user registration

--- datahub/datahub/urls.py
+++ datahub/datahub/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from user_registration import urls as user_reg
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^register/', include()),
+    url(r'^register/', include(user_reg)),
 ]
