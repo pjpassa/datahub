3005d926f6a96d0af5141063f87e7a9da3c57dcc
PJ Passalacqua <pjpassa@gmail.com>
2015-08-07 10:51:11 -0400

    updates to api

--- datahub/data_analysis/serializers.py
+++ datahub/data_analysis/serializers.py
@@ -9,15 +9,28 @@ class DatasetSerializer(BaseSerializer):
         self.page_size = kwargs.pop('page_size', 100)
         super().__init__(*args, **kwargs)
 
-
     def to_representation(self, instance):
+        columns = self.context["request"].query_params.get("columns", None)
+        if columns:
+            for column in instance.data.columns:
+                print(column)
+            return [{"data": column} for column in instance.data.columns]
         page = int(self.context["request"].query_params.get("page", self.page_number))
-        if not self.page_number:
-            return OrderedDict([('dataset', instance.name),
-                                ('data', instance.data)])
+        num_obs = len(instance.data.index)
         start = (page - 1) * self.page_size
-        end = start + self.page_size
+        end = min(start + self.page_size, num_obs)
+        next_page = None
+        prev_page = None
+        if end < num_obs:
+            next_page = instance.api_link + "?page=" + str(page + 1)
+        if start > 0:
+            prev_page = instance.api_link + "?page=" + str(page - 1)
+        data = [OrderedDict(row) for _, row in instance.data.iloc[start:end].iterrows()]
         return OrderedDict([('dataset', instance.name),
                             ('page', page),
-                            ('columns', instance.data.columns),
-                            ('data', instance.data.iloc[start:end])])
+                            ('page_size', self.page_size),
+                            ('next_page', next_page),
+                            ('prev_page', prev_page),
+                            ('num_obs', num_obs),
+                            # ('columns', instance.data.columns),
+                            ('data', data)])
